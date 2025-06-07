03b47fab9ce8f8f95e1f5da1143faeb6507e6576
flow-of-water <nguyenleviethoang62@gmail.com>
2025-06-08 00:08:36 +0700

    config DB for production

--- EnglishForce-backend/src/sequelize/config/config.js
+++ EnglishForce-backend/src/sequelize/config/config.js
@@ -13,9 +13,9 @@ export default {
     dialect: "postgres"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD_PRODUCTION,
+    database: process.env.DB_NAME,
     host: "127.0.0.1",
     dialect: "postgres",
     logging: false,
