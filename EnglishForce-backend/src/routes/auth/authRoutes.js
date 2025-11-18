00822940d81e2993826c681ea56817f38dda1975
flow-of-water <nguyenleviethoang62@gmail.com>
2025-11-18 23:54:50 +0700

    fix auth rate limiter

--- EnglishForce-backend/src/routes/auth/authRoutes.js
+++ EnglishForce-backend/src/routes/auth/authRoutes.js
@@ -4,9 +4,10 @@ import { authMiddleware } from "../../middleware/authorize.js";
 import rateLimiter from "../../middleware/rateLimit.js";
 
 const router = express.Router();
+const authLimiter = rateLimiter(100);
 
-router.post("/register", rateLimiter(maxRequest=50), register);
-router.post("/login", rateLimiter(maxRequest=50), login);
+router.post("/register", authLimiter, register);
+router.post("/login", authLimiter, login);
 router.patch("/change-password",authMiddleware, changePassword) ;
 router.post("/refresh-token",refreshToken ) ;
 
